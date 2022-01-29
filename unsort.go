74fe1f4195aaea107c2e5a787d9e557388f6fa7c
0x1a8510f2 <admin@0x1a8510f2.space>
2022-01-29 00:58:07 +0000

    Initial unsorting algorithm

--- unsort.go
+++ unsort.go
@@ -1,24 +1,32 @@
 package main
 
 import (
-//	"math/rand"
+	"math/rand"
+	"time"
 	"bufio"
 	"fmt"
 	"os"
 	"strings"
 )
 
-func unsort(list []string) []string {
-	newlist := []string{}
-	
-	return newlist
+// Init PRNG
+func init() { rand.Seed(time.Now().UnixNano()) }
+
+// Unsorting algorithm
+func Unsort(list []string) []string {
+	for range list {
+		swapindex1, swapindex2 := rand.Intn(len(list)), rand.Intn(len(list))
+		list[swapindex1], list[swapindex2] = list[swapindex2], list[swapindex1]
+	}
+	return list
 }
 
+// Unsort wrapper for demo purposes
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
 	fmt.Print("Enter a space-delimeted list: ")
 	scanner.Scan()
 	liststr := scanner.Text()
 	list := strings.Split(liststr, ",")
-	fmt.Printf("%v\n", unsort(list))
+	fmt.Printf("%v\n", Unsort(list))
 }
