d8b003902fe0d35cd1786139f3c00553af9a15e2
nikolashn <nehn.2019@gmail.com>
2022-03-11 00:30:33 +0000

    Add benchmarking tests for both functions

--- unsort_test.go
+++ unsort_test.go
@@ -4,45 +4,36 @@ import (
 	"fmt"
 	"math"
 	"encoding/json"
-	"flag"
 	"testing"
 )
 
-type unsortfuncpair struct {
-	name string
-	Run func([]string) []string
+var sorted = []string{"0","1","2","3","4","5","6","7","8","9"}
+var counter = map[string]int{
+	"0": 0, 
+	"1": 0, 
+	"2": 0,
+	"3": 0, 
+	"4": 0, 
+	"5": 0, 
+	"6": 0, 
+	"7": 0, 
+	"8": 0, 
+	"9": 0,
 }
 
-var ufindex = flag.Int("func", 0, "Specify the unsort function to be used by its index. 0=BasicUnsort, 1=OneSwapIndexUnsort")
-
-func TestUnsort(t *testing.T) {
-	unsortoptions := []unsortfuncpair {
-		{"Basic unsort", BasicUnsort},
-		{"One swap index unsort", OneSwapIndexUnsort},
+func UnsortTestTemplate (b *testing.B, name string, Unsort func([]string)[]string) {
+	for k, _ := range counter {
+		counter[k] = 0
 	}
-	sorted := []string{"0","1","2","3","4","5","6","7","8","9"}
-	counter := map[string]int{
-		"0": 0,
-		"1": 0,
-		"2": 0,
-		"3": 0,
-		"4": 0,
-		"5": 0,
-		"6": 0,
-		"7": 0,
-		"8": 0,
-		"9": 0,
+	fmt.Println(name)
+	b.StartTimer()
+	for j := 0; j < b.N; j++ {
+		for i := 0; i < 100000; i++ {
+			unsorted := Unsort(sorted)
+			counter[unsorted[0]]++
+		}
 	}
-	if *ufindex < 0 || *ufindex > 1 {
-		t.Errorf("Invalid unsort function index")
-		return
-	}
-	unsortfunc := unsortoptions[*ufindex]
-	fmt.Printf("--- %s ---\n", unsortfunc.name)
-	for i := 100000; i > 0; i-- {
-		unsorted := unsortfunc.Run(sorted)
-		counter[unsorted[0]]++
-	}
+	b.StopTimer()
 	leastvalue := ""
 	leastcount := math.Inf(1)
 	mostvalue := ""
@@ -61,6 +52,15 @@ func TestUnsort(t *testing.T) {
 	}
 	tenpercent := mostcount / 10
 	if float64(mostcount - tenpercent) > leastcount {
-		t.Errorf("%s (most common @ %d) is more common than %s (least common @ %d) by over 10 percent", mostvalue, mostcount, leastvalue, int(leastcount))
+		b.Errorf("%s (most common @ %d) is more common than %s (least common @ %d) by over 10 percent", mostvalue, mostcount, leastvalue, int(leastcount))
 	}
 }
+
+func BenchmarkBasicUnsort(b *testing.B) {
+	UnsortTestTemplate(b, "Basic unsort", BasicUnsort);
+}
+
+func BenchmarkOneSwapIndexUnsort(b *testing.B) {
+	UnsortTestTemplate(b, "One swap index unsort", OneSwapIndexUnsort);
+}
+
